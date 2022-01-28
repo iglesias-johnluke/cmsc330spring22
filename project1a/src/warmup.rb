e4d61f0eb8174894c48d57d919ea5bd16de8f7ca
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-01-28 18:17:23 -0500

    part 1 done

--- project1a/src/warmup.rb
+++ project1a/src/warmup.rb
@@ -22,21 +22,65 @@ def fib(n)
 end
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+    return n.to_s == n.to_s.reverse
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+    if n > a.length() - 1
+        return nil
+    else
+        a.sort!
+        a.reverse!
+        return a[n]
+    end
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+    output = ""
+    if s == ""
+        return output
+    else
+        charArray = s.split('')
+        hashMap = Hash.new
+        charArray.each { |char| #loop thru chars, map char to num occurences in map
+            if hashMap.key?(char) 
+                hashMap[char] += 1
+            else
+                hashMap[char] = 1
+            end
+        }
+        maxCount = hashMap.values.max #get largest count in map, find corresponding key
+        hashMap.keys.each { |char|
+            if hashMap[char] == maxCount
+                return char
+            end
+        }
+    end
 end
 
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+    hashMap = {}
+    if arr1.length == 0 and arr2.length == 0 
+        return hashMap
+    elsif arr1.length != arr2.length
+        return nil
+    else
+        index = 0
+        for item in arr1 do
+            hashMap[item] = arr2[index]
+            index += 1 
+        end
+        return hashMap
+    end
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+    output = []
+    if hash.keys.length != 0
+        hash.keys.each{ |key|
+            output.push( [key, hash[key]] )
+
+        }
+    end
+    return output
 end
