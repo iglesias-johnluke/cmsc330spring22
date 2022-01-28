2bea29e58448ab94ddb742014ace4d2c7f941ae9
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-01-28 16:52:47 -0500

    fib()

--- project1a/src/warmup.rb
+++ project1a/src/warmup.rb
@@ -1,5 +1,24 @@
 def fib(n)
-    raise Exception, "Not Implemented"
+    num1 = 0
+    num2 = 1
+    count = 0
+    arr = Array.new(0)
+    if n == 1
+        arr.push(0)
+    elsif n == 2
+        arr.push(0, 1)
+    elsif n > 2
+        arr.push(0, 1)
+        count = 2
+        while count < n
+            newNum = num1 + num2
+            arr.push(newNum)
+            num1 = num2
+            num2 = newNum
+            count += 1
+        end   
+    end
+    return arr
 end
 
 def isPalindrome(n)
