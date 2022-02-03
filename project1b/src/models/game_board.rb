67b1c14093cc19a5acf10bfb3b6d05b3a2eeba4c
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-02-03 17:54:36 -0500

    board to_s

--- project1b/src/models/game_board.rb
+++ project1b/src/models/game_board.rb
@@ -3,9 +3,14 @@ class GameBoard
     # @max_column is an `Integer`
     attr_reader :max_row, :max_column
 
+    #grid is array of arrays (rows and columns)
+    #each sub-array/column items are hashes with keys "isShip" and "attacked"
+    #mapping to boolean values
     def initialize(max_row, max_column)
         @max_row = max_row
         @max_column = max_column
+        @grid = Array.new(max_row){Array.new(max_column){ 
+            {"isShip" => false, "attacked" => false} } }
     end
 
     # adds a Ship object to the GameBoard
@@ -13,7 +18,9 @@ class GameBoard
     # Returns true on successfully added the ship, false otherwise
     # Note that Position pair starts from 1 to max_row/max_column
     def add_ship(ship)
-        true
+        # puts "ship row: #{ship.start_position.row} ship column: #{ship.start_position.column}"
+        @grid[ship.start_position.row - 1][ship.start_position.column - 1]["isShip"] = true
+        return true
     end
 
     # return Boolean on whether attack was successful or not (hit a ship?)
@@ -39,9 +46,30 @@ class GameBoard
         true
     end
 
-
     # String representation of GameBoard (optional but recommended)
     def to_s
-        "STRING METHOD IS NOT IMPLEMENTED"
+        output = "    "
+        for col in 1..max_column do #print column numbers
+            output += "#{col}" + "    "
+        end
+        output += "\n"
+        for row in 0..(max_row - 1) do #print rows/arrays
+            output += "#{row + 1}: "
+            for col in 0..(max_column - 1) do
+                if @grid[row][col]["isShip"] == false
+                    output += "-,"
+                else
+                    output += "B,"
+                end
+                if @grid[row][col]["attacked"] == false
+                    output += "-"
+                else
+                    output += "A"
+                end
+                output += "| "
+            end
+            output += "\n"
+        end
+        return output
     end
 end
