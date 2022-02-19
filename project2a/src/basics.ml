df88c6dbb9d643bc4fc2036e560b0cf0f484ca91
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-02-18 20:10:17 -0500

    p2a rough draft

--- project2a/src/basics.ml
+++ project2a/src/basics.ml
@@ -1,6 +1,7 @@
 (***********************************)
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
+open Format
 
 let rev_tup tup = let (a, b, c) = tup in (c, b, a)
 
@@ -34,30 +35,108 @@ let volume x y =
 (* Part 2: Recursive Functions *)
 (*******************************)
 
-let rec fibonacci n = failwith "unimplemented"
+let rec fibonacci n = 
+    if n = 1 then
+        1
+    else if n = 2 then
+        1
+    else
+        fibonacci (n-1) + fibonacci (n-2)
 
-let rec pow x y = failwith "unimplemented"
+let rec pow x y = 
+    if y = 1 then
+        x
+    else
+        x * pow x (y-1)
 
-let rec log x y = failwith "unimplemented"
+let rec log x y = 
+    if (y / x) = 1 then
+        1
+    else if y < x then
+        0
+    else
+        1 + log x (y/x)
+    
+let rec gcf x y = 
+    if y = 0 then
+        x
+    else
+        gcf y (x mod y)
 
-let rec gcf x y = failwith "unimplemented"
+let rec isPrimeAux num currDivisor =
+    if num = 1 then
+        false
+    else if currDivisor >= num then
+        true
+    else if (num mod currDivisor) = 0 then
+        false
+    else
+        isPrimeAux num (currDivisor+1)
 
-let rec is_prime x = failwith "unimplemented"
+let rec is_prime x = 
+    isPrimeAux x 2
+
 
 (*****************)
 (* Part 3: Lists *)
 (*****************)
 
-let rec get idx lst = failwith "unimplemented"
+let rec get_aux idx lst curr = 
+    if idx = curr then
+        match lst with
+            | [] -> failwith "Out of bounds"
+            | (first::_) -> first
+    else
+        match lst with
+            | [] -> failwith "Out of bounds"
+            | (_::rest) -> get_aux idx rest (curr+1)
 
-let larger lst1 lst2 = failwith "unimplemented"
+let rec get idx lst = get_aux idx lst 0
 
-let reverse lst = failwith "unimplemented"
 
-let rec combine lst1 lst2 = failwith "unimplemented"
+let rec getLength lst total =
+    match lst with
+            | [] -> total
+            | (_::rest) -> getLength rest (total+1)
 
-let rec merge lst1 lst2 = failwith "unimplemented"
+let larger lst1 lst2 = 
+    if (getLength lst1 0) = (getLength lst2 0) then lst1 else
+    if (getLength lst1 0) > (getLength lst2 0) then lst1 else lst2
 
-let rec rotate shift lst = failwith "unimplemented"
+let rec reverseAux lst (newList:'a list) = 
+    match lst with
+        | [] -> newList
+        | (first::rest) -> reverseAux rest (first::newList)
 
-let rec is_palindrome lst = failwith "unimplemented"
+let reverse lst = reverseAux lst []
+
+
+let rec combine lst1 lst2 = 
+    match lst1 with 
+        [] -> lst2  (*list1 empty, return list2, which appends list2 to list1 *)
+        (* recursively reach list 1's last element, then base case appends list2*)
+        | (first :: rest) -> (first :: (combine rest lst2) ) 
+
+let rec merge lst1 lst2 = 
+    lst2
+
+let rec insert_at_end l i =
+  match l with
+    [] -> [i]
+  | h :: t -> h :: (insert_at_end t i)
+
+let rec getSecondHalf shift oldList newList currIndex =
+    if currIndex = shift - 1 then
+        newList
+    else
+        let newList = insert_at_end(newList (get currIndex oldList)) in
+            (getSecondHalf shift oldList newList (currIndex+1))
+
+
+let rec rotate shift lst = 
+    let output = getSecondHalf (shift lst [] 0) in
+    output
+
+let rec is_palindrome lst = 
+    let reversedList = reverse lst in
+    lst = reversedList
